de144f56adeb899d609b233cee7cea4e4182ce7f
komalappdev <komal.appdev@gmail.com>
2024-03-13 18:25:37 +0530

    initial commit

---
+++ lib/src/app_theme.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+
+ThemeMode currentTheme = ThemeMode.light;
+
+class ChangeAppTheme extends StatefulWidget {
+  const ChangeAppTheme({super.key});
+
+  @override
+  State<ChangeAppTheme> createState() => _ChangeAppThemeState();
+}
+
+class _ChangeAppThemeState extends State<ChangeAppTheme> {
+  var brightness =
+      SchedulerBinding.instance.platformDispatcher.platformBrightness;
+
+  @override
+  Widget build(BuildContext context) {
+    return Switch(
+      value: false,
+      onChanged: (value) {
+        if (value && brightness == Brightness.light) {
+          setCurrentTheme(ThemeMode.dark);
+        } else {
+          setCurrentTheme(ThemeMode.light);
+        }
+      },
+    );
+  }
+
+  void setCurrentTheme(ThemeMode theme) {
+    currentTheme = theme;
+  }
+}
