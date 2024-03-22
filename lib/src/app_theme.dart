f725b85891314a9240fb1f3f263a508722f06da9
komalappdev <komal.appdev@gmail.com>
2024-03-22 12:39:05 +0530

    add widget for open dialog

--- lib/src/app_theme.dart
+++ lib/src/app_theme.dart
@@ -8,10 +8,12 @@ enum TypeSetting { typeSwitch, typeDialog }
 class ChangeAppTheme extends StatefulWidget {
   final OnThemeChangeCallback? onThemeChangeCallback;
   final TypeSetting typeSetting;
+  final String textValue;
   const ChangeAppTheme(
       {super.key,
       required this.onThemeChangeCallback,
-      required this.typeSetting});
+      required this.typeSetting,
+      required this.textValue});
 
   @override
   State<ChangeAppTheme> createState() => _ChangeAppThemeState();
@@ -25,49 +27,6 @@ class _ChangeAppThemeState extends State<ChangeAppTheme> {
   @override
   void initState() {
     super.initState();
-    if (widget.typeSetting == TypeSetting.typeDialog) {
-      showDialog(
-        context: context,
-        builder: (context) {
-          return Dialog(
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(12.0)), //this right here
-            child: SizedBox(
-              height: 300.0,
-              width: 300.0,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  const Padding(
-                    padding: EdgeInsets.all(15.0),
-                    child: Text(
-                      'Light',
-                      style: TextStyle(color: Colors.red),
-                    ),
-                  ),
-                  const Padding(
-                    padding: EdgeInsets.all(15.0),
-                    child: Text(
-                      'Dark',
-                      style: TextStyle(color: Colors.red),
-                    ),
-                  ),
-                  const Padding(padding: EdgeInsets.only(top: 50.0)),
-                  TextButton(
-                      onPressed: () {
-                        Navigator.of(context).pop();
-                      },
-                      child: const Text(
-                        'System Default',
-                        style: TextStyle(color: Colors.purple, fontSize: 18.0),
-                      ))
-                ],
-              ),
-            ),
-          );
-        },
-      );
-    }
   }
 
   @override
@@ -85,7 +44,57 @@ class _ChangeAppThemeState extends State<ChangeAppTheme> {
               setState(() {});
             },
           )
-        : Container();
+        : TextButton(
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (context) {
+                  return Dialog(
+                    shape: RoundedRectangleBorder(
+                        borderRadius:
+                            BorderRadius.circular(12.0)), //this right here
+                    child: SizedBox(
+                      height: 300.0,
+                      width: 300.0,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          const Padding(
+                            padding: EdgeInsets.all(15.0),
+                            child: Text(
+                              'Light',
+                              style: TextStyle(color: Colors.red),
+                            ),
+                          ),
+                          const Padding(
+                            padding: EdgeInsets.all(15.0),
+                            child: Text(
+                              'Dark',
+                              style: TextStyle(color: Colors.red),
+                            ),
+                          ),
+                          const Padding(padding: EdgeInsets.only(top: 50.0)),
+                          TextButton(
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                              child: const Text(
+                                'System Default',
+                                style: TextStyle(
+                                    color: Colors.purple, fontSize: 18.0),
+                              ))
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              );
+            },
+            child: Text(
+              widget.textValue,
+              style: Theme.of(context).primaryTextTheme.titleSmall,
+            ),
+          );
   }
 }
 
